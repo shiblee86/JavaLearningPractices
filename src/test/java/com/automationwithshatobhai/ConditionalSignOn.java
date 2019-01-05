9d806dcccdf067a693604652b402785fc3bb04e5
Syed Abdullah <syed.r.abdullah@gmail.com>
2019-01-05 16:32:11 -0500

    New project

---
+++ src/test/java/com/automationwithshatobhai/ConditionalSignOn.java
@@ -0,0 +1,53 @@
+package com.automationwithshatobhai;
+
+public class ConditionalSignOn {
+
+	public static void main(String[] args) {
+
+		boolean signOnDisplayed = false;
+
+		if (signOnDisplayed == true) {
+			System.out.println("Username sent");
+		} else {
+			System.out.println("Username is not displayed");
+		}
+
+		String tabButton = "one";
+
+		switch (tabButton) {
+
+		case "one":
+			System.out.println("Hotel");
+			break;
+		case "two":
+			System.out.println("Flights");
+			break;
+		case "three":
+			System.out.println("Hotel + Flight");
+			break;
+		case "four":
+			System.out.println("Cars");
+			break;
+		case "five":
+			System.out.println("Cruises");
+			break;
+		default:
+			System.out.println("There is no match");
+
+		}
+
+		int i;
+		for (i = 1; i <= 2;) {
+			if (i > 1) {
+				System.out.println("Option 2");
+			} else {
+				System.out.println("Option 1");
+			}
+			i++;
+		}
+		String macPath = System.getProperty("user.home");
+		System.out.println(macPath);
+
+	}
+
+}
