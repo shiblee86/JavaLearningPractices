85542491eaaabc6fae1bf48db462a3e092450750
Syed Abdullah <syed.r.abdullah@gmail.com>
2019-01-31 22:10:40 -0500

    Field value passing from method to method.

--- src/test/java/com/bookpractice/EnglishSpeakingWorld2.java
+++ src/test/java/com/bookpractice/EnglishSpeakingWorld2.java
@@ -24,3 +24,10 @@ class EnglishSpeakingWorld2 {
 		// System.out.println(atomicCity);
 	}
 }
+
+/*When EnglishSpeakingWorld2 class first flashes into existence, the computer sees String mars and creates space for the mars field/instance variable
+*When visitIdaho() method is executed, the method assigns value " red planet" to the mars field. (The visitIdaho() method also prints the value of the mars field
+*When visitNJ() method is executed, the method accepts mars value from visitIdaho method and prints the mars value once again
+*Since mars is not initiated again inside visitNJ() method, the value of mars is passed from visitIdaho() to visitNJ()
+*mars field value is passed from from one method to another.
+*If we change the value of mars inside visitNJ(), visitNJ() methods assigns the new value to mars and prints the new value*/
